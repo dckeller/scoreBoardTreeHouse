0f829d696688a49014c285b9c993c8b771702ac6
Keller <david.keller@RMD-MAC-109.local>
2018-11-27 16:11:25 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './Header';
+import Player from './Player';
 
 class App extends Component {
+
+  state = {
+    players: [
+    {
+      name: "Guil",
+      score: 0,
+      id: 1
+    },
+    {
+      name: "Treasure",
+      score: 0,
+      id: 2
+    },
+    {
+      name: "Ashley",
+      score: 0,
+      id: 3
+    },
+    {
+      name: "James",
+      score: 0,
+      id: 4
+    }
+    ]
+  };
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta   
+    }));
+  }
+
+  handleDeletePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter( p => p.id !== id)    
+      }
+    });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="scoreboard">
+      <Header 
+      title="Scoreboard" 
+      totalPlayers={ this.state.players.length }
+      />
+
+    {/* JavaScript Iteration */}
+    {this.state.players.map( (player, index) => 
+      <Player
+      name={player.name}
+      score={player.score}
+      id={player.id}
+      changeScore={this.handleScoreChange}
+      key={player.id.toString()}
+      index={index}
+      removePlayer={this.handleDeletePlayer}
+      /> 
+      )}
+    </div>
     );
   }
 }
